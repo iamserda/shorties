5b9e72269a57d09a0462c959d470f1d02bec6ff2
iamserda <12580399+iamserda@users.noreply.github.com>
2026-01-30 02:49:21 -0500

    feat: can retrieve based on keys using url params. Has basic error handling. Logging errors later.  --@iamserda

--- src/app/main.py
+++ src/app/main.py
@@ -5,7 +5,18 @@ from app.constants import KEY_MAX
 
 
 app = FastAPI()
-store = {}
+store = {
+    "goog": "https://www.google.com",
+    "meta": "https://www.facebook.com",
+    "twttr": "https://www.twitter.com",
+    "lknd": "https://www.linkedin.com",
+    "github": "https://www.github.com",
+    "reddit": "https://www.reddit.com",
+    "youtub": "https://www.youtube.com",
+    "insta": "https://www.instagram.com",
+    "microsoft": "https://www.microsoft.com",
+    "apple": "https://www.apple.com",
+}
 
 
 @app.get("/healthz")
@@ -13,26 +24,65 @@ def healthz() -> dict:
     return {"status": "alive"}
 
 
-@app.get("/shorten")
-def shorten() -> dict:
+@app.get("/redirect/{key}")
+def get_url(key: str) -> dict:
+    try:
+        if not key:
+            raise ValueError("Invalid, user did not provide a key.")
+        if not isinstance(key, str):
+            raise TypeError("The key provided is not of a valid type. Key must be a str.")
+        if key in store:
+            return {"key": f"{key}", "url": store.get(key), "status": "success", "message": "success: url was found!"}
+        raise KeyError("failure: this key does not match our records. Verify the key and try again.")
+    except KeyError as err:
+        print(err)  # todo: log error, send failure message, suggestions for retrying.
+        return {
+            "key": f"{key}",
+            "url": None,
+            "status": "failure",
+            "message": f"key-not-found-error: {err}",
+        }
+    except ValueError as err:
+        print(f"invalid-data-error: {err}")  # todo: log error, send failure message, suggestions for retrying.
+        return {
+            "key": f"{key}",
+            "url": None,
+            "status": "failure",
+            "message": f"value-not-found-error: {err}",
+        }
+    except TypeError as err:
+        print(f"invalid-data-error: {err}")  # todo: log error, send failure message, suggestions for retrying.
+        return {
+            "key": f"{key}",
+            "url": None,
+            "status": "failure",
+            "message": f"type-validity-error: {err}",
+        }
+
+
+@app.post("/create/{new_url}")
+def create_url(new_url) -> dict:
     key = alnum_generator()
-    while not key.isalnum():
-        key = alnum_generator()
-    if store.get(key):
-        return {"key": f"{key}", "url": store[key], "status": "success", "message": "A key was successfully generated"}
-
-    return {"key": f"{key}", "url": None, "status": "success", "message": "A key was successfully generated"}
-
-
-@app.post("/create")
-def create_url():
-    key = alnum_generator()
-    while not 
+    try:
+        while key in store:
+            key = alnum_generator()
+        store[key] = new_url
+        if key in store:
+            return {
+                "key": f"{key}",
+                "url": store[key],
+                "status": "success",
+                "message": "A key was successfully generated",
+            }
+        else:
+            return {"key": f"{key}", "url": None, "status": "failed", "message": "A key was successfully generated"}
+    except KeyError as err:
+        print(err)  # todo: logg
+        pass
 
 
 if __name__ == "__main__":
     import uvicorn
-
     HOST: str = "0.0.0.0"
     PORT: int = 8001
     uvicorn.run(app, host=HOST, port=PORT, log_level="debug")
