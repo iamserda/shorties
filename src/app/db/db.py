26e55de12be2e127c46afa82366419f8d72c15f5
iamserda <12580399+iamserda@users.noreply.github.com>
2026-02-12 03:25:22 -0500

    feat: #1: introducing a file-based RDMS via SQLite. Current model is extremely basic to get things working quickly. Will add both analytics data-table, and more columns to the current shortilink table.
    #2: streamlined database engine creation and improved error handling;
    
    #3L removed unused code and simplified main app initialization
    
    --@iamserda

--- src/app/db/db.py
+++ src/app/db/db.py
@@ -10,15 +10,20 @@ def db_engine_factory(db_url: str, dev_mode=False):
     try:
         if db_url is None:
             raise ValueError("A URL path is required.")
-        return create_engine(db_url, echo=dev_mode)
+        db_engine = create_engine(db_url, echo=dev_mode)
+        if db_engine is None:
+            raise ValueError(
+                "Failed to create DB ENGINE. Check the URL argument. Check sys/admin/dev logs for more details!"
+            )
+        return db_engine
     except ValueError as valErr:
-        print("url-path-error:", valErr)
+        print("url-path-error:", valErr)  # TODO: Logger!
         return None
 
 
 if __name__ == "__main__":
     DATABASE_URL = os.getenv("DATABASE_URL")
-    DEV_ENV = os.getenv("DEV_ENV")
+    DEV_ENV: bool = os.getenv("DEV_ENV", "False") == "True"
     if DATABASE_URL:
         db_engine = db_engine_factory(db_url=DATABASE_URL, dev_mode=DEV_ENV)
         if db_engine:
--- src/app/main.py
+++ src/app/main.py
@@ -1,52 +1,29 @@
 from __future__ import annotations
 
-from app.alnumgen import alnum_generator
-from app.schemas.schemas import URLRequestModel
-from app.schemas.schemas import UrlResponseModel
+import os
+
+from app.db.db import db_engine_factory
+from app.db.models.models import ShortiLink
 from fastapi import APIRouter
 from fastapi import FastAPI
-from fastapi import HTTPException
-from pydantic import AnyUrl
+from sqlmodel import select
+from sqlmodel import Session
+from sqlmodel import SQLModel
 
+DATABASE_URL = "sqlite:///src/app/db/SHORTIES_DATABASE.db"
+DEV_ENV: bool = os.getenv("DEV_ENV", "False") == "True"
+db_engine = db_engine_factory(db_url=DATABASE_URL, dev_mode=DEV_ENV)
+
+if db_engine:
+    SQLModel.metadata.create_all(db_engine)
 
 app = FastAPI(title="Shorties App")
 api_router = APIRouter()
-api_version = "/v1"
-shorti_links: dict = {
-    "scap": {"brand": "scap", "url": "https://www.scapital.com"},
-    "goog": {"brand": "goog", "url": "https://www.google.com"},
-    "meta": {"brand": "meta", "url": "https://www.facebook.com"},
-    "twit": {"brand": "twit", "url": "https://www.twitter.com"},
-    "link": {"brand": "link", "url": "https://www.linkedin.com"},
-    "gith": {"brand": "gith", "url": "https://www.github.com"},
-    "redd": {"brand": "redd", "url": "https://www.reddit.com"},
-    "yout": {"brand": "yout", "url": "https://www.youtube.com"},
-    "inst": {"brand": "inst", "url": "https://www.instagram.com"},
-    "micr": {"brand": "micr", "url": "https://www.microsoft.com"},
-    "appl": {"brand": "appl", "url": "https://www.apple.com"},
-    "amaz": {"brand": "amaz", "url": "https://www.amazon.com"},
-    "netf": {"brand": "netf", "url": "https://www.netflix.com"},
-    "spof": {"brand": "spof", "url": "https://www.spotify.com"},
-    "slac": {"brand": "slac", "url": "https://www.slack.com"},
-    "drop": {"brand": "drop", "url": "https://www.dropbox.com"},
-    "adob": {"brand": "adob", "url": "https://www.adobe.com"},
-    "uber": {"brand": "uber", "url": "https://www.uber.com"},
-    "airb": {"brand": "airb", "url": "https://www.airbnb.com"},
-    "tesl": {"brand": "tesl", "url": "https://www.tesla.com"},
-    "nyti": {"brand": "nyti", "url": "https://www.nytimes.com"},
-    "wash": {"brand": "wash", "url": "https://www.washingtonpost.com"},
-    "bbc": {"brand": "bbc", "url": "https://www.bbc.com"},
-    "cnn": {"brand": "cnn", "url": "https://www.cnn.com"},
-    "espn": {"brand": "espn", "url": "https://www.espn.com"},
-    "pint": {"brand": "pint", "url": "https://www.pinterest.com"},
-    "tumblr": {"brand": "tumblr", "url": "https://www.tumblr.com"},
-    "quor": {"brand": "quor", "url": "https://www.quora.com"},
-    "yaho": {"brand": "yaho", "url": "https://www.yahoo.com"},
-    "ebay": {"brand": "ebay", "url": "https://www.ebay.com"},
-    "payp": {"brand": "payp", "url": "https://www.paypal.com"},
-    "tikt": {"brand": "tikt", "url": "https://www.tiktok.com"},
-}
-click_event: dict = {}
+api_version = os.getenv("API_VERSION")
+
+
+if not api_version:
+    api_version = "/v1"
 
 
 @api_router.get(f"{api_version}/healthz/")
@@ -55,104 +32,105 @@ def healthz() -> dict:
 
 
 @api_router.get(f"{api_version}/display/")
-def display_all(max_results: int | None = None) -> dict:
-    store = shorti_links
-    if max_results is None or max_results <= 0 or max_results >= len(store):
-        return store
-    else:
-        results = {}
-        for i, (k, v) in enumerate(store.items()):
-            if i >= max_results:
-                break
-            else:
-                results[k] = v
-        return results
-
-
-@api_router.get(f"{api_version}/redirect/")
-def get_url(key: str) -> dict:
-    store = shorti_links
+def display_all() -> list:
     try:
-        if not key:
-            raise ValueError("Invalid, user did not provide a key.")
-        if not isinstance(key, str):
-            raise TypeError(
-                "The key provided is not of a valid type. Key must be a str.",
-            )
-        if key in store:
-            return {
-                "key": f"{key}",
-                "url": store.get(key),
-                "status": "success",
-                "message": f'success: url matching "{key}"  was found!',
-            }
-        else:
-            raise HTTPException(
-                status_code=404,
-                detail="failure: this key does not match our records. Verify the key and try again.",
-            )
-
-    except HTTPException as err:
-        # todo: log error, send failure message, suggestions for retrying.
-        print(err)
-        raise err
-    except ValueError as err:
-        # todo: log error, send failure message, suggestions for retrying.
-        print(f"invalid-data-error: {err}")
-        return {
-            "key": f"{key}",
-            "url": None,
-            "status": "failure",
-            "message": f"value-not-found-error: {err}",
-        }
-    except TypeError as err:
-        # todo: log error, send failure message, suggestions for retrying.
-        print(f"invalid-data-error: {err}")
-        return {
-            "key": f"{key}",
-            "url": None,
-            "status": "failure",
-            "message": f"type-validity-error: {err}",
-        }
+        if not db_engine:
+            raise ValueError("Error with DB Engine!")
+        with Session(db_engine) as session:
+            select_statement = select(ShortiLink)
+            results = session.exec(statement=select_statement).all()
+            for r in results:
+                print(r)
+            return list(results)
+    except Exception as err:
+        print("Error:", err)
+        return []
 
 
-@api_router.post(f"{api_version}/create/")
-def create_url(url_item: URLRequestModel) -> UrlResponseModel:
-    store = shorti_links
-    key = alnum_generator()
-    print(url_item)
-    try:
-        while key in store:
-            key = alnum_generator()
+# @api_router.get(f"{api_version}/redirect/")
+# def get_url(key: str) -> dict:
+#     store = shorti_links
+#     try:
+#         if not key:
+#             raise ValueError("Invalid, user did not provide a key.")
+#         if not isinstance(key, str):
+#             raise TypeError(
+#                 "The key provided is not of a valid type. Key must be a str.",
+#             )
+#         if key in store:
+#             return {
+#                 "key": f"{key}",
+#                 "url": store.get(key),
+#                 "status": "success",
+#                 "message": f'success: url matching "{key}"  was found!',
+#             }
+#         else:
+#             raise HTTPException(
+#                 status_code=404,
+#                 detail="failure: this key does not match our records. Verify the key and try again.",
+#             )
 
-        new_brand = url_item.brand
-        new_url = url_item.url
-        store[key] = {"brand": new_brand, "url": new_url}
-        new_url_item = UrlResponseModel(
-            key=key,
-            brand=store[key]["brand"],
-            url=AnyUrl(store[key]["url"]),
-            status="success",
-            message="A key was successfully generated",
-        )
-        if key in store:
-            return new_url_item
+#     except HTTPException as err:
+#         # todo: log error, send failure message, suggestions for retrying.
+#         print(err)
+#         raise err
+#     except ValueError as err:
+#         # todo: log error, send failure message, suggestions for retrying.
+#         print(f"invalid-data-error: {err}")
+#         return {
+#             "key": f"{key}",
+#             "url": None,
+#             "status": "failure",
+#             "message": f"value-not-found-error: {err}",
+#         }
+#     except TypeError as err:
+#         # todo: log error, send failure message, suggestions for retrying.
+#         print(f"invalid-data-error: {err}")
+#         return {
+#             "key": f"{key}",
+#             "url": None,
+#             "status": "failure",
+#             "message": f"type-validity-error: {err}",
+#         }
 
-        # failed = UrlResponseModel(
-        #     key=key,
-        #     brand="",
-        #     url="",
-        #     status="failure",
-        #     message="We could not create a new key at this moment. Please try again later!",
-        # )
-        # return failed
-        raise HTTPException(
-            status_code=404,
-            detail="status='failure', message='We could not create a new key at this moment!'",
-        )
-    except HTTPException as err:
-        print(err)  # todo: logg
-        raise err
+
+# @api_router.post(f"{api_version}/create/")
+# def create_url(url_item: URLRequestModel) -> UrlResponseModel:
+#     store = shorti_links
+#     key: str = alnum_generator()
+#     print(url_item)
+#     try:
+#         while key in store:
+#             key = alnum_generator()
+
+#         new_brand: str = url_item.brand
+#         new_url: str = url_item.url
+#         store[key]: dict = {"brand": new_brand, "url": new_url}
+#         new_url_item = UrlResponseModel(
+#             key=key,
+#             brand=store[key]["brand"],
+#             url=AnyUrl(store[key]["url"]),
+#             status="success",
+#             message="A key was successfully generated",
+#         )
+#         if key in store:
+#             return new_url_item
+
+#         # failed = UrlResponseModel(
+#         #     key=key,
+#         #     brand="",
+#         #     url="",
+#         #     status="failure",
+#         #     message="We could not create a new key at this moment. Please try again later!",
+#         # )
+#         # return failed
+#         raise HTTPException(
+#             status_code=404,
+#             detail="status='failure', message='We could not create a new key at this moment!'",
+#         )
+#     except HTTPException as err:
+#         print(err)  # todo: logg
+#         raise err
 
 
 app.include_router(api_router)
