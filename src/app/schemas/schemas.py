1ea01a4924122e1aa90db62746d20abcc5c1a026
iamserda <12580399+iamserda@users.noreply.github.com>
2026-06-16 22:22:35 -0400

    refactor: simplify response model and update URL validation in schemas

--- src/app/schemas/schemas.py
+++ src/app/schemas/schemas.py
@@ -2,17 +2,14 @@ from __future__ import annotations
 
 from typing import Literal
 
-from pydantic import AnyUrl
+from pydantic import AnyHttpUrl
 from pydantic import BaseModel
 
 
 class NewUrlSubmissionModel(BaseModel):
     brand: str | None = None
-    url: AnyUrl | str
-    expires_on: str | None = None
-    redirect_code: Literal[301, 302] = 302
-    tags: list[str] | None = []
-    notes: str | None = None
+    url: AnyHttpUrl | str = "https://i.imgur.com/Secssr2.png"
+    redirect_code: Literal[301, 302, 307] = 307
 
     def __str__(self):
         return f"brand: {self.brand}, url: {self.url}"
@@ -24,11 +21,5 @@ class GetURLRequestModel(BaseModel):
 
 class GetUrlResponseModel(BaseModel):
     key: str | None = None
+    url: str | None = None
     brand: str | None = None
-    url: AnyUrl | str
-    status: str | None = None
-    message: str | None = None
-    expires_on: str | None = None
-    redirect_code: Literal[301, 302] = 302
-    tags: list[str] | None = []
-    notes: str | None = None
