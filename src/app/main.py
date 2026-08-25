3b327f24fe4d5ad847f2d8e70e7214b986b8a9ce
iamserda <12580399+iamserda@users.noreply.github.com>
2026-08-25 06:49:01 -0400

    Refactor API version handling in FastAPI app configuration

--- src/app/main.py
+++ src/app/main.py
@@ -38,10 +38,9 @@ if db_engine:
 # FASTAPI APP Config
 app = FastAPI(title="Shorties App")
 api_router = APIRouter()
-api_version = f"/{os.getenv('API_VERSION')}"
-if api_version not in set(["v1", "v2", "v3", "v4", "v5"]):
+api_version = str(os.getenv("API_VERSION"))
+if api_version not in {"v1", "v2", "v3", "v4", "v5"}:
     api_version = "v1"
-
 
 api_router.include_router(links.router)
 api_router.include_router(health.router)
